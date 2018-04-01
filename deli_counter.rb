15328077ed98334a500b1c9c38f207019320f65d
Zhengxiang Gong <jessiegong92@gmail.com>
2018-04-01 03:38:01 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,7 +3,7 @@ def line(katz_deli)
     if katz_deli.empty?
         puts "The line is currently empty."
     else
-        result = "The line is currently: "
+        result = "The line is currently:"
         katz_deli.each_with_index { |customer, index|
             result << "#{index + 1}. #{customer}"
 
