c7cb41d8e0333f61fcd468de6e07b258d65cb999
Zhengxiang Gong <jessiegong92@gmail.com>
2018-04-01 03:44:04 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -17,3 +17,7 @@ def take_a_number(katz_deli, customer)
 end
 
 def now_serving(katz_deli)
+    if katz_deli.empty?
+        puts "There is nobody waiting to be served!"
+    else
+        puts "Currently serving #{katz_deli.pop}"
